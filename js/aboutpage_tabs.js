af1d193962b723d14a58364ab3c0a38119eaef37
zero09285 <wuchyng1111@gmail.com>
2020-07-14 13:58:06 +0800

    07/14 Update Ver.1

--- js/aboutpage_tabs.js
+++ js/aboutpage_tabs.js
@@ -5,3 +5,38 @@ $(".tab-btn").click(function () {
     $(this).addClass('tab_active')
     $(`.tab-content:nth-child(${btn_index + 1}`).addClass('tab_active')
 })
+
+$(".map01").click(function () {
+  
+    if ($('#Kiln01').addClass('noactive')) {
+        $('#Kiln01').removeClass('noactive')
+    }else {
+        $('#Kiln01').addClass('noactive')
+    }
+})
+$(".map02").click(function () {
+
+    if ($('#Kiln02').addClass('noactive')) {
+        $('#Kiln02').removeClass('noactive')
+    }else {
+        $('#Kiln02').addClass('noactive')
+    }
+})
+   
+$(".map03").click(function () {
+    if ($('#Kiln03').addClass('noactive')) {
+        $('#Kiln03').removeClass('noactive')
+    }else {
+        $('#Kiln03').addClass('noactive')
+    }
+})
+   
+$(".map04").click(function () {
+    if ($('#Kiln04').addClass('noactive')) {
+        $('#Kiln04').removeClass('noactive')
+    }else {
+        $('#Kiln04').addClass('noactive')
+    }
+})
+    
+
