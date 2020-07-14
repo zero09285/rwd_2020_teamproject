4cf1c33c845c9573388dd397b27bd7fb1c261b95
zero09285 <wuchyng1111@gmail.com>
2020-07-14 15:04:15 +0800

    07/14 Update Ver.3

--- js/aboutpage_tabs.js
+++ js/aboutpage_tabs.js
@@ -6,37 +6,43 @@ $(".tab-btn").click(function () {
     $(`.tab-content:nth-child(${btn_index + 1}`).addClass('tab_active')
 })
 
-$(".map01").click(function () {
+$(".map01").mouseover(function () {
   
-    if ($('#Kiln01').addClass('noactive')) {
-        $('#Kiln01').removeClass('noactive')
-    }else {
-        $('#Kiln01').addClass('noactive')
-    }
+    $(".box01").css('display', 'block')
 })
-$(".map02").click(function () {
 
-    if ($('#Kiln02').addClass('noactive')) {
-        $('#Kiln02').removeClass('noactive')
-    }else {
-        $('#Kiln02').addClass('noactive')
-    }
+$(".map01").mouseout(function () {
+  
+    $(".box01").css('display', 'none')
+})
+$(".map02").mouseover(function () {
+  
+    $(".box02").css('display', 'block')
+})
+
+$(".map02").mouseout(function () {
+  
+    $(".box02").css('display', 'none')
 })
    
-$(".map03").click(function () {
-    if ($('#Kiln03').addClass('noactive')) {
-        $('#Kiln03').removeClass('noactive')
-    }else {
-        $('#Kiln03').addClass('noactive')
-    }
+$(".map03").mouseover(function () {
+  
+    $(".box03").css('display', 'block')
+})
+
+$(".map03").mouseout(function () {
+  
+    $(".box03").css('display', 'none')
 })
    
-$(".map04").click(function () {
-    if ($('#Kiln04').addClass('noactive')) {
-        $('#Kiln04').removeClass('noactive')
-    }else {
-        $('#Kiln04').addClass('noactive')
-    }
+$(".map04").mouseover(function () {
+  
+    $(".box04").css('display', 'block')
+})
+
+$(".map04").mouseout(function () {
+  
+    $(".box04").css('display', 'none')
 })
     
 
