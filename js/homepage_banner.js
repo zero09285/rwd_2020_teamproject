05094df1ae9378a717c3646e3f60bd9572df2b4a
zero09285 <wuchyng1111@gmail.com>
2020-07-14 16:33:03 +0800

    07/14 Update Ver.5

--- js/homepage_banner.js
+++ js/homepage_banner.js
@@ -1,15 +1,15 @@
-window.onload = function () {
-    lax.setup()
-    const updateLax = () => {
-        lax.update(window.scrollY)
-        window.requestAnimationFrame(updateLax)
-    }
-    window.requestAnimationFrame(updateLax)
-}
+// window.onload = function () {
+//     lax.setup()
+//     const updateLax = () => {
+//         lax.update(window.scrollY)
+//         window.requestAnimationFrame(updateLax)
+//     }
+//     window.requestAnimationFrame(updateLax)
+// }
 
-var section = document.querySelector("#banner")
+// var section = document.querySelector("#banner")
 
-section.onscroll = function () {
-    console.log(section.sc);
+// section.onscroll = function () {
+//     console.log(section.sc);
 
-}
+// }
